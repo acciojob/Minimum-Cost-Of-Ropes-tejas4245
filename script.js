9373fe08802cc466e44d640a0a0101c1c85269da
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-02-17 15:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your code here
+function minimumCost(){
+	var res=0;
+	var cost=0;
+	var inputArray=document.getElementByld("inputArr").value;
+	inputArray=inputArray.split(',').map(Number);
+	for(let i=0;i<inputArray.length-1;i+=0){
+		if(inputArray.length==0) break;
+		inputArray=inputArray.sort((a,b))=>b-a);
+		res=inputArray.pop+inputArray.pop();
+		cost+=res;
+		inputArray[inputArray.length]=res;
+	}
+	document.getElementByld("result").innerHTML=cost;
+}
+	
+}
