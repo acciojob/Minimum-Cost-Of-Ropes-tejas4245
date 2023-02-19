65c3a35ad57576aab02139d24c8e7cd3f770f8b4
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-02-19 15:35:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function minimumCost(){
 	var res=0;
 	var cost=0;
-	var inputArray=document.getElementByld("inputArr").value;
+	var inputArray=document.getElementById("inputArr").value;
 	inputArray=inputArray.split(',').map(Number);
 	for(let i=0;i<inputArray.length-1;i+=0){
 		if(inputArray.length==0) break;
 		inputArray=inputArray.sort((a,b))=>b-a);
-		res=inputArray.pop+inputArray.pop();
+		res=inputArray.pop()+inputArray.pop();
 		cost+=res;
 		inputArray[inputArray.length]=res;
 	}
-	document.getElementByld("result").innerHTML=cost;
-}
-	
+	document.getElementById("result").innerHTML=cost;
 }
