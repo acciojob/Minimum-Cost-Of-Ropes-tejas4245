13c496fbdcb19c917d1b2ea9283db8196bfc6d10
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-17 15:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function CalculateMinCost(){
 	var inputData= Document querySelector("#repo-lengths");
-	var inputarr= inputData.split(",");
+	var inputArr= inputData.split(",");
 	
-	for(i=0;i<inputArr.length;i++){
+	for(var i=0;i<inputArr.length;i++){
 		inputArr[i]=Number(inputArr[i]);
 	}
 	var cost=0;
 	inputArr.sort(function(a,b) {return a-b});
 	while (inputArr.length>1) {
 		var newRepo=inputArr[0]+inputArr[1];
-		cost+=newRepo;
+		cost+=newRope;
 		inputArr.splice(0,2);
-		inputArr.push(newRepo);
+		inputArr.push(newRope);
 		inputArr.sort(function(a,b){return a-b});
 		
 	}
